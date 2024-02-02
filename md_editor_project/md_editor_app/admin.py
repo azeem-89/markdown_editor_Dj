701235de54f15cd2fe75392f4e2b8ca296f9272b
Deepika-45 <deepikashukla201725@gmail.com>
2024-02-02 10:50:15 +0530

    .\md_editor_project\

---
+++ md_editor_project/md_editor_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Edit_Doc
+
+admin.site.register(Edit_Doc)
+
---
+++ md_editor_project/md_editor_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+
+# markdown_editor_app/views.py
+from django.shortcuts import render
+from .forms import Edit_Doc_Form
+import markdown
+
+def markdown_editor(request):
+    if request.method == 'POST':
+        form = Edit_Doc_Form(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            html_content = markdown.markdown(content)
+            return render(request, 'preview.html', {'title': title, 'html_content': html_content})
+    else:
+        form = Edit_Doc_Form()
+
+    return render(request, 'md_editor.html', {'form': form})
